c29780955869e29b1709c52c13b885a86fb8ef5d
Paulus Robin <paulus.robin@hotmail.com>
2022-07-31 14:39:22 +0400

    chore: update runtime config

--- internal/config/configRuntime.go
+++ internal/config/configRuntime.go
@@ -1,6 +1,11 @@
 package config
 
-import "sync"
+import (
+	consulViper "github.com/paulusrobin/gogen-golib/remote-config/consul/integrations/viper"
+	consul "github.com/paulusrobin/gogen-golib/remote-config/consul/interface"
+	"github.com/pkg/errors"
+	"sync"
+)
 
 type (
 	businessConfig struct {
@@ -13,4 +18,24 @@ var runtimeBusinessConfig = newBusinessConfig()
 func newBusinessConfig() businessConfig {
 	// fallback values
 	return businessConfig{}
+}
+
+func provideRuntimeConfig(cfg Config) {
+	reader, err := consulViper.NewConsulReader(consul.Config{
+		Connection: consul.ConnectionConfig{
+			Host:  cfg.Consul.Host,
+			Port:  cfg.Consul.Port,
+			Token: cfg.Consul.Token,
+			Key:   cfg.Consul.Key,
+		},
+		ConfigType: "json",
+		Interval:   cfg.Consul.RefreshInterval,
+	})
+	if err != nil {
+		panic(errors.Wrap(err, "cannot initialize consul reader"))
+	}
+
+	if err = reader.Read(&runtimeBusinessConfig); err != nil {
+		panic(errors.Wrap(err, "cannot initialize consul business config variable"))
+	}
 }
--- internal/config/provider.go
+++ internal/config/provider.go
@@ -1,44 +1,18 @@
 package config
 
 import (
-	consulViper "github.com/paulusrobin/gogen-golib/remote-config/consul/integrations/viper"
-	consul "github.com/paulusrobin/gogen-golib/remote-config/consul/interface"
-	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
 // InitConfig initializes configuration from .env file and returns config structure.
 func InitConfig() Config {
-	var (
-		cfg    Config
-		reader consul.Reader
-		err    error
-	)
-
-	cfg, err = readConfigFromEnv()
+	cfg, err := readConfigFromEnv()
 	if err != nil {
 		panic("cannot initialize .env config")
 	}
 
-	reader, err = consulViper.NewConsulReader(consul.Config{
-		Connection: consul.ConnectionConfig{
-			Host:  cfg.Consul.Host,
-			Port:  cfg.Consul.Port,
-			Token: cfg.Consul.Token,
-			Key:   cfg.Consul.Key,
-		},
-		ConfigType: "json",
-		Interval:   cfg.Consul.RefreshInterval,
-	})
-	if err != nil {
-		panic(errors.Wrap(err, "cannot initialize consul reader"))
-	}
-
-	if err = reader.Read(&runtimeBusinessConfig); err != nil {
-		panic(errors.Wrap(err, "cannot initialize consul business config variable"))
-	}
-
+	provideRuntimeConfig(cfg)
 	return cfg
 }
 
